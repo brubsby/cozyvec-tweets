2a8bb433809a514d860c3bfbe4ba9750b16e81e7
brubsby <brubsbybrubsby@gmail.com>
2020-02-17 22:32:17 -0600

    launch tweets

---
+++ icon.js
@@ -0,0 +1,8 @@
+//icon #cozyvec #plottertwitter
+paper("Postcard",0)
+pen(.25)
+f=rnd(.005,.04)
+a=rnd(3,9)
+o=flr(rnd(1,3.25))
+Y=rnd(100,200)
+for(i=0;i<Y;i++){t=0;for(j=0;j<(X=999);j++){y=H*i/Y;x=W*j/X;y=y+nse([x,y],f,a,o);if(abs(W/2-x)+abs(H/2-y)<min(W,H)/4){(t++?l2:m2)(x,y)}else{t=0}}}
---
+++ sphoise.js
@@ -0,0 +1,2 @@
+//sphoise #cozyvec #plottertwitter
+b=mbox();w=b[2]-b[0];h=b[3]-b[1];r=min(w/2,h/2);X=999;Y=99;for(i=0;i<Y;i++){y=h*i/Y+b[1];for(j=0;j<=X;j++){x=w*j/X+b[0];(j?l2:m2)(x,y+nse([x,y],1,9)*smid(0,r-dst(W/2,H/2,x,y),r)/r)}}
