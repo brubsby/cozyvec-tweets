b9237bd4c9d48cd539bb0de77e36a56f7760deca
brubsby <57653502+brubsby@users.noreply.github.com>
2020-02-26 16:47:30 -0600

    Create fribbon.js

---
+++ fribbon.js
@@ -0,0 +1,2 @@
+//fribbon #cozyvec #plottertwitter
+l=[...Array((F=9)+1)].map((_,j)=>((n,s,y,w,z)=>[...Array(n+3)].map((_,i)=>[x=s+i*(w-s)/(n+2),(y*(w-x)+z*(x-s))/(w-s)]))(L=99,rnd((b=mbox())[0],W/2),Y=(b[3]-b[1])*j/F+b[1],rnd(W/2,b[2]),Y));for(i=0;i<L;i++)for(k=0;k<F+1;k++)(k?l2:m2)(...l[k][i])
